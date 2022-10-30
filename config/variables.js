aceddbc15d625b4d0025e62562a9844244216c33
Ckai <ckai.trans@gmail.com>
2022-10-30 23:00:40 +0800

    feat: add batch display stock price function

--- config/variables.js
+++ config/variables.js
@@ -4,7 +4,7 @@ const variables = {
   urlStockIdBase: 'https://goodinfo.tw/tw/StockDetail.asp?STOCK_ID=',
   // XPath
   xPathClosingPrice: '/html/body/table[2]/tbody/tr/td[3]/table/tbody/tr[1]/td/table/tbody/tr/td[1]/table/tbody/tr[3]/td[1]',
-  // 股票代號
+  // 股票代號。注意！上述 urlStockIdBase 加上 0056 與 加上 56 所得的結果並不相同。
   50: '0050', // 元大台灣50
   56: '0056', // 元大高股息
   2324: '2324' // 仁寶
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { Builder, By, until } = require('selenium-webdriver')
 const variables = require('./config/variables')
 
-async function browserAutomation () {
+async function browserAutomation (stockArray) {
   const driver = await new Builder().forBrowser('chrome').build()
-  await driver.get(`${variables.urlStockIdBase}${variables[50]}`)
-  const closingPrice = await driver.wait(until.elementLocated(By.xpath(variables.xPathClosingPrice))).getText()
-  console.log(closingPrice)
+  for (const stock of stockArray) {
+    await driver.get(`${variables.urlStockIdBase}${variables[stock]}`)
+    const closingPrice = await driver.wait(until.elementLocated(By.xpath(variables.xPathClosingPrice))).getText()
+    console.log(closingPrice)
+  }
 }
 
-browserAutomation()
+browserAutomation([50, 56, 2324])
