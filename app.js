057d2361ed6f47c1d893abf4340969807a471337
Ckai <ckai.trans@gmail.com>
2022-11-11 22:17:58 +0800

    feat: add express and express-handlebars

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const exphbs = require('express-handlebars')
 const routes = require('./routes')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3456
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
