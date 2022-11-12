3ffc991bab74a220225adb34d9df509d1773f75f
Ckai <ckai.trans@gmail.com>
2022-11-12 21:58:14 +0800

    feat: add node-schedule and set schedule

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
+require('dotenv').config()
 const { Builder } = require('selenium-webdriver')
 const { disableGpu, headless, imageOff, shmUsage } = require('./config/chrome-options')
 const crawler = require('./tools/crawler')
 const dbCRUD = require('./tools/data-base-crud')
 
 async function browserAutomation () {
-  console.log('爬蟲開始任務')
+  console.info('爬蟲開始任務')
   try {
     const stocks = await dbCRUD.getStocks()
     // 以無頭模式等設定啟動瀏覽器
     const driver = await new Builder().setChromeOptions(headless, imageOff, disableGpu, shmUsage).forBrowser('chrome').build()
     await crawler.getPrices(driver, stocks)
-    console.log('爬蟲結束任務')
+    console.info('爬蟲結束任務')
     await driver.quit()
   } catch (err) { console.error(err) }
 }
 
-browserAutomation()
+module.exports = browserAutomation
---
+++ tools/schedule.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const { scheduleJob } = require('node-schedule')
+const browserAutomation = require('../index')
+let isCrawlerRun = false // 用以檢查爬蟲是否執行中
+
+console.info('排程套件啟用中')
+
+scheduleJob(process.env.SCHEDULE_TIME, async () => {
+  try {
+    if (!isCrawlerRun) { // 程式運作期間僅執行一隻爬蟲
+      isCrawlerRun = true
+      console.info(`排程作業啟動爬蟲 ${new Date()}`)
+      await browserAutomation()
+      isCrawlerRun = false
+      console.info('排程作業執行完畢')
+    }
+  } catch (err) { console.error(err) }
+})
